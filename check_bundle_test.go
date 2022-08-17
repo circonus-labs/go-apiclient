5c1bbaae423d89621f24d838cae1682722074061
maier <maier@users.noreply.github.com>
2022-08-17 11:00:34 -0400

    fix: scoped var in range

--- check_bundle_test.go
+++ check_bundle_test.go
@@ -501,6 +501,7 @@ func Test_fixTags(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			if got := fixTags(tt.tags); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("fixTags() = %v, want %v", got, tt.want)
